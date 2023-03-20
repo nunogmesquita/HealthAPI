3320bc56f16d69c96e80ead7faf6dda95e4d603c
Nuno Gil Mesquita <nunomesquita2@gmail.com>
2023-03-20 15:19:00 +0000

    Initial commit

---
+++ src/main/java/mindswap_2023/HealthApiApplication.java
@@ -0,0 +1,13 @@
+package mindswap_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mindswap_2023/HealthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mindswap_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
